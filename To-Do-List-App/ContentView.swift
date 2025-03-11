8dadbb85948e7e7f0ab7d5b98ebaf95b62957d88
ashitosh39 <151606213+ashitosh39@users.noreply.github.com>
2025-03-11 15:53:15 +0530

    Initial Commit

---
+++ To-Do-List-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  To-Do-List-App
+//
+//  Created by Digitalflake on 11/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ To-Do-List-App/To_Do_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_List_AppApp.swift
+//  To-Do-List-App
+//
+//  Created by Digitalflake on 11/03/25.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
